1538d4506619c0092c4802c422e5b00455f6e659
sofia-papa <papa.sofia@outlook.it>
2021-09-13 17:44:50 +0100

    functions items

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,5 @@
 //Milestone 1:
-//Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+//Partendo dalla seguente struttura dati, mostriamo in pagina tutte le icone disponibili come da layout.
 
 //Milestone 2:
 //Coloriamo le icone per tipo
@@ -8,7 +8,7 @@
 //Creiamo una select con i tipi di icone e usiamola per filtrare le icone
 
 
-const object = [
+const icons = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -106,3 +106,71 @@ const object = [
 		family: 'fas'
 	}
 ];
+
+//milestone 1
+
+let containerIcons = document.getElementById("my_icon-box");
+console.log(containerIcons);
+
+arrayColored = itemsColored (icons, colors);
+seeOnPage(arrayColored, container);
+
+const types = getTypes(icons, "type");
+const select = document.getElementById("my_select");
+
+console.log(getTypes(icons, "type"));
+addOptions(types, select);
+
+
+
+//FUNCTIONS
+
+function seeOnPage(array, container){
+    let htmlContent = "";
+    
+    array.forEach((element) => {
+        const {family, name, prefix, type} = element;
+
+        htmlContent += 
+        `<div>
+            <i class= "${family} ${prefix}${name}">
+            style = "color: ${color}"></i>
+            <h4 class="my_icon-title>${name} ${type} </h4>
+        </div>`   
+    });
+    container.innerHTML = htmlContent;   
+}
+
+function getTypes(array){
+    const types = [];
+
+    array.forEach((element,index) => {
+        if(! types.includes(element.type)){
+            types.push(element.type);
+        }
+    });
+
+    return types;
+}
+
+function itemsColored (array, colors) {
+    const types = getTypes (array, "type");
+    console.log(types);
+
+    const arrayColored = array.map ((element) => {
+        const indexofType = types.indexOf(element.type);
+
+        if (indexofType !== -1){
+            element.color = colors[indexOfTypes];
+        }
+        return element;
+    })
+    return itemsColored;
+}
+
+function addOptions(options, select){
+    options.forEach((element) => {
+        select.innerHTML +=  `<option value="${element}">${element}</option>`;
+    });
+    console.log(select);
+}
